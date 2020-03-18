b270f2305ece70835d8d5587c65f60f56abc0f2a
GraemeHollingsworth <31771338+GraemeHollings@users.noreply.github.com>
2020-03-18 10:36:45 -0400

    Began JavaFX UI implementation.

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -5,15 +5,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
 
 public class Main extends Application {
 
+    //private TextField
+
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        GridPane panel = new GridPane();
+        panel.setHgap(5);
+        panel.setVgap(5);
+
+
+
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Traffic Citations - Sault Ste. Marie");
         primaryStage.setScene(new Scene(root, 300, 275));
+
         primaryStage.show();
+
+
     }
 
 
